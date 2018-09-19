5b5a1f18c8669d289f49f64120f60e00635653d8
lightswarm124 <lightswarm@gmail.com>
2018-09-19 16:28:53 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+
+const wrap = require('./wrap');
+
+module.exports = async () => {
+  let blockHeight = await axios.get('https://rest.bitcoin.com/v1/blockchain/getBlockCount')
+    .then(result => {
+      return result.data;
+    })
+    .catch(err => {
+      return err;
+    });
+
+  let merkleRoot = await axios.get(`https://rest.bitcoin.com/v1/block/details/${blockHeight}`)
+    .then(result => {
+      return result.data.merkleroot;
+    })
+    .catch(err => {
+      return err;
+    });
+
+  return await {
+    bkh: blockHeight,
+    mkr: merkleRoot,
+    ebn: blockHeight + 1
+  };
+};
